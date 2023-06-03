55880624572da06e24381eb808d5c346db24df29
Nguyễn Long Thành <106393870+Nlt2498@users.noreply.github.com>
2023-06-03 15:23:40 +0700

    Update Master_I2C.ino

--- EMBEDDED/I2C/Master_I2C.ino
+++ EMBEDDED/I2C/Master_I2C.ino
@@ -1,3 +1,10 @@
+/*
+ * File: Master.ino
+ * Author: NGUYEN THANH
+ * Date: 5/5/2023
+ * Description: This is file for MASTER_I2C using Arduino
+*/
+
 #define SCL_PIN 3
 #define SDA_PIN 4
 
@@ -10,11 +17,36 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  uint8_t result;
-  result = I2C_Write_Data(0x55, "012345");  //-> 0xAA
+  
+  //MASTER READ DATA FROM SLAVE---------------------------
+  byte rev[20]={0};
+  int8_t res;
+  res = I2C_ReadData(0x13, rev, 7);
+  if(res==1){
+    Serial.print("Read ok: ");
+    Serial.println(String((char*)rev));
+  }else if(res==-1){
+    Serial.println("Read fails: ");
+  }
+  delay(1000);
+
+  
+  //MASTER SEND DATA TO SLAVE------------------------------------
+  int8_t result;
+  result = I2C_WriteData(0x13,"[M]12345.:)"); // S AB A 31 A 32 A 33 A 34 A 35 A P,
+  // 0x56 = 1010.1100
   Serial.println(result);
-  delay(200);
+  delay(1000); 
 }
+
+/*
+ * Function: I2C_start
+ * Description: This function use for starting I2C signal
+ * Input:
+ *  input: none
+ * Output:
+ *  return: none 
+*/
 
 void I2C_Start() {
   digitalWrite(SDA_PIN, HIGH);
@@ -26,6 +58,15 @@ void I2C_Start() {
   digitalWrite(SCL_PIN, LOW);
 }
 
+/*
+ * Function: I2C_stop
+ * Description: This function use for stopping I2C signal
+ * Input:
+ *  input: none
+ * Output:
+ *  return: none 
+*/
+
 void I2C_Stop()
 {
   pinMode(SDA_PIN, OUTPUT);
@@ -36,10 +77,19 @@ void I2C_Stop()
   digitalWrite(SDA_PIN, HIGH); 
 }
 
+/*
+ * Function: I2C_ReadACK
+ * Description: This function use for reading ACK signal from Salve about
+ * Input:
+ *  input: none
+ * Output:
+ *  return: 1 bit ACK 
+*/
+
 uint8_t I2C_ReadACK(void) {
   uint8_t ACK;
-  pinMode(SDA_PIN, INPUT); //SLAVE HAVE SDA BUS
-  delayMicroseconds(500);// CLK 9
+  pinMode(SDA_PIN, INPUT);  //SLAVE HAVE SDA BUS
+  delayMicroseconds(500);   // CLK 9
   digitalWrite(SCL_PIN, HIGH);
   delayMicroseconds(500);
   ACK = digitalRead(SDA_PIN); //LSB 
@@ -48,8 +98,17 @@ uint8_t I2C_ReadACK(void) {
   //SLAVE RELEASE SDA BUS
   delayMicroseconds(500);
   pinMode(SDA_PIN, OUTPUT);  
-  return ACK; //ACK(0);NACK(1)
+  return ACK;   //ACK(0); NACK(1)
 }
+
+/*
+ * Function: I2C_Write_Data
+ * Description: This function use for MaSter to transfer data I2C.
+ * Input:
+ *   data - data want to send and adrress
+ * Output:
+ *   return: 1 Success or -1 Fail
+*/
 
 uint8_t I2C_Write_Data(byte address, char *data)
 {
@@ -78,6 +137,16 @@ uint8_t I2C_Write_Data(byte address, char *data)
    I2C_Stop();
    return 1;  
 }
+
+
+/*
+ * Function: I2C_Write_8bits
+ * Description: This function use for write 8 bit data.
+ * Input:
+ *   data - data want to send
+ * Output:
+ *   return: none
+*/
 
 void I2C_Write_8bits(byte data) {
   int i;  
